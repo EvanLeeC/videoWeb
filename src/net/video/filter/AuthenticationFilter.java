e162dde256184c704b9d113a6046a27c04a010e7
lixiaoyan <lixiaoyan@zai360.com>
2017-05-16 13:46:02 +0800

    权限

--- src/net/video/filter/AuthenticationFilter.java
+++ src/net/video/filter/AuthenticationFilter.java
@@ -34,20 +34,9 @@ public class AuthenticationFilter extends FormAuthenticationFilter {
 	/** 默认"加密密码"参数名称 */
 	private static final String DEFAULT_EN_PASSWORD_PARAM = "enPassword";
 
-	/** 默认"验证ID"参数名称 */
-	private static final String DEFAULT_CAPTCHA_ID_PARAM = "captchaId";
-
-	/** 默认"验证码"参数名称 */
-	private static final String DEFAULT_CAPTCHA_PARAM = "captcha";
-
 	/** "加密密码"参数名称 */
 	private String enPasswordParam = DEFAULT_EN_PASSWORD_PARAM;
 
-	/** "验证ID"参数名称 */
-	private String captchaIdParam = DEFAULT_CAPTCHA_ID_PARAM;
-
-	/** "验证码"参数名称 */
-	private String captchaParam = DEFAULT_CAPTCHA_PARAM;
 
 	@Resource(name = "rsaServiceImpl")
 	private RSAService rsaService;
@@ -115,44 +104,6 @@ public class AuthenticationFilter extends FormAuthenticationFilter {
 	 */
 	public void setEnPasswordParam(String enPasswordParam) {
 		this.enPasswordParam = enPasswordParam;
-	}
-
-	/**
-	 * 获取"验证ID"参数名称
-	 * 
-	 * @return "验证ID"参数名称
-	 */
-	public String getCaptchaIdParam() {
-		return captchaIdParam;
-	}
-
-	/**
-	 * 设置"验证ID"参数名称
-	 * 
-	 * @param captchaIdParam
-	 *            "验证ID"参数名称
-	 */
-	public void setCaptchaIdParam(String captchaIdParam) {
-		this.captchaIdParam = captchaIdParam;
-	}
-
-	/**
-	 * 获取"验证码"参数名称
-	 * 
-	 * @return "验证码"参数名称
-	 */
-	public String getCaptchaParam() {
-		return captchaParam;
-	}
-
-	/**
-	 * 设置"验证码"参数名称
-	 * 
-	 * @param captchaParam
-	 *            "验证码"参数名称
-	 */
-	public void setCaptchaParam(String captchaParam) {
-		this.captchaParam = captchaParam;
 	}
 
 }
